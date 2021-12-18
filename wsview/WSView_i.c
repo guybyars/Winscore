cbc28e52354b8410188a969345b8b537ee6ab31f
guybyars <96307927+guybyars@users.noreply.github.com>
2021-12-18 08:41:35 -0500

    chore(): update installer and cleanup

--- wsview/WSView_i.c
+++ wsview/WSView_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Dec 17 19:09:00 2021
+/* at Fri Dec 17 20:08:49 2021
  */
 /* Compiler settings for WSView.odl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
